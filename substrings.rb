e101995a4eb025371d11d2bae0d8214d55560891
ElKekso <159456154+ElKekso@users.noreply.github.com>
2024-11-18 09:45:20 +0100

    Change include? to scan

--- substrings.rb
+++ substrings.rb
@@ -1,16 +1,12 @@
 def substrings(string, dictionary)
-  string = string.downcase
-  string_arr = string.split(' ')
-  hash = Hash.new
+  string_arr = string.downcase.split(' ')
+  hash = Hash.new(0)
 
   for dic_word in dictionary
     for str_word in string_arr
-      if str_word.include?(dic_word) then
-        if hash[dic_word].nil? then
-           hash[dic_word] = 1
-          next
-        end
-        hash[dic_word] += 1
+      amount = str_word.scan(dic_word).length
+      if amount > 0 then
+        hash[dic_word] += amount
       end
     end
   end
